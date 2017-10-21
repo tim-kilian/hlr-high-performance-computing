e63691ae52e79cba6c809fe6acf8176a8bd7938f
5kilian <5kilian@informatik.uni-hamburg.de>
2017-10-21 23:44:57 +0200

    Add Ergänzungen und Abgabe

--- 01-C/map/map.c
+++ 01-C/map/map.c
@@ -8,6 +8,7 @@ typedef enum { N=1, W=2, E=4, S=8 } cardd;
 // Definieren Sie ein 3x3-Array namens map, das Werte vom Typ cardd enthält
 static cardd map[3][3];
 
+// Übersetzt die cardinal directions in ihre entsprechende symbolische Repräsentation
 char* get_dir(cardd dir) {
     switch (dir) {
         case N: return "N";
@@ -18,6 +19,7 @@ char* get_dir(cardd dir) {
     }
 }
 
+// Formatiert die jeweiligen acht Himmelsrichtungen.
 char* format_dir(cardd dir) {
     char* c = malloc(3);
 
@@ -32,23 +34,22 @@ char* format_dir(cardd dir) {
 // Die Funktion set_dir soll an Position x, y den Wert dir in das Array map eintragen
 // Überprüfen Sie x und y um mögliche Arrayüberläufe zu verhindern
 // Überprüfen Sie außerdem dir auf Gültigkeit
-void set_dir (int x, int y, cardd dir) {
+void set_dir(int x, int y, cardd dir) {
     if (x < 0 || x > 2 || y < 0 || y > 2) {
-        printf("Error: set_dir x=%d, y=%d - out of range\n", x, y);
+//        printf("Error: set_dir x=%d, y=%d - out of range\n", x, y);
         return;
-    }
-    else if (dir == (N|S) || dir == (W|E)) {
-        printf("Error: set_dir dir=%s - direction not supported\n", format_dir(dir));
+    } else if (dir == (N|S) || dir == (W|E)) {
+//        printf("Error: set_dir dir=%s - direction not supported\n", format_dir(dir));
         return;
     }
     map[x][y] = dir;
 }
 
 // Die Funktion show_map soll das Array in Form einer 3x3-Matrix ausgeben
-void show_map (void) {
+void show_map(void) {
     for (int i=0; i<3; i++) {
         char c[3][3];
-        for (int j=0; j<3; ++j) {
+        for (int j=0; j<3; j++) {
             strcpy(c[j], format_dir(map[i][j]));
         }
         printf("%-4s%s%4s\n", c[0], c[1], c[2]);
