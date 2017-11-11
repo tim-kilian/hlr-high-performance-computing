644f59763a3697eb30417807a3fe779ede9557d3
Joscha Fregin <fregin@wr.informatik.uni-hamburg.de>
2017-11-11 20:52:08 +0100

    Aufgabe 3 unfertig

--- 03-PDE/pde/partdiff-seq.c
+++ 03-PDE/pde/partdiff-seq.c
@@ -225,14 +225,14 @@ calculate (struct calculation_arguments* arguments, struct calculation_results *
 		maxresiduum = 0;
 
 		/* over all rows */
-		for (j = 1; j < N; j++)
+		for (i = 1; i < N; i++)
 		{
 			/* over all columns */
-			for (i = 1; i < N; i++)
+			for (j = 1; j < N; j++)
 			{
 				star = -Matrix[m2][i-1][j] - Matrix[m2][i][j-1] - Matrix[m2][i][j+1] - Matrix[m2][i+1][j] + 4.0 * Matrix[m2][i][j];
 
-				residuum = getResiduum(arguments, options, i, j, star);
+				residuum = getResiduum(arguments, options, i, j, star); /* residuum = ((-star)/4.0); spart ca 1.7 sec ohne compiler optimierung */
 				korrektur = residuum;
 				residuum = (residuum < 0) ? -residuum : residuum;
 				maxresiduum = (residuum < maxresiduum) ? maxresiduum : residuum;
